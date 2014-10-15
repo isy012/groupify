18d684d6ecdae86e2c41b46fc975ba91ce1edd25
Izzy Park <isy012@hotmail.com>
2014-10-15 18:00:46 -0400

    crud operations for group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,15 +1,70 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:show, :edit, :update, :destroy]
+
   def show
-  	@group = Group.find(params[:id])
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @group }
-      format.json { render :json => @group }
-    end
+  	#@group = Group.find(params[:id])
+    #respond_to do |format|
+    #  format.html # index.html.erb
+    #  format.xml  { render :xml => @group }
+    #  format.json { render :json => @group }
+    #end
   end
 
   def index
     @group = Group.order('created_at DESC').all
     @user = User.all
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def edit
+  end
+
+  def create
+    @group = Group.new(group_params)
+
+    respond_to do |format|
+      if @group.save
+        format.html { redirect_to @group, notice: 'Group was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @group }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @group.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @group.update(group_params)
+        format.html { redirect_to @group, notice: 'Group was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @group.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @group.destroy
+    respond_to do |format|
+      format.html { redirect_to groups_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_group
+      @group = Group.find_by_id(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def group_params
+      params.require(:group).permit(:title, :content, :seats, :when)
+    end
+
 end
